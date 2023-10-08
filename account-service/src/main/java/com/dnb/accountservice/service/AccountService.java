a24f423332379831d8a48200e56cb2d87c708ff4
Bhuvana V <99163228+BhuvanaVangari@users.noreply.github.com>
2023-10-08 23:50:13 +0530

    acc service

---
+++ account-service/src/main/java/com/dnb/accountservice/service/AccountService.java
@@ -0,0 +1,30 @@
+package com.dnb.accountservice.service;
+
+import java.util.Optional;
+
+import com.dnb.accountservice.dto.Account;
+import com.dnb.accountservice.exceptions.AccountCloseException;
+import com.dnb.accountservice.exceptions.IdNotFoundException;
+import com.dnb.accountservice.exceptions.InsufficientBalanceException;
+
+public interface AccountService {
+	public Account createAccount(Account account) throws IdNotFoundException;
+	
+	public Optional<Account>getAccountById(String accountId) throws IdNotFoundException;
+	
+	public Iterable<Account>getAllAccounts();
+	
+	public Optional<Account>getAccountByUserId(Integer userId);
+	
+	public boolean accountExistsById(String accountId);
+	
+	public boolean deleteAccountbyId(String accountId)throws IdNotFoundException;
+	
+	public void deleteAccountByUserId(Integer userId);
+
+	public Account depositAmount(String accountId, long balance) throws IdNotFoundException, AccountCloseException;
+
+	public Account withdrawAmount(String accountId, long balance) throws IdNotFoundException, InsufficientBalanceException, AccountCloseException;
+
+	public Account closeAccount(String accountId)throws IdNotFoundException,AccountCloseException;
+}
