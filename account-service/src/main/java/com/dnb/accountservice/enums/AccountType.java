136ce918d0dcea0cba4e6499aa17ce36639b43c3
Bhuvana V <99163228+BhuvanaVangari@users.noreply.github.com>
2023-10-09 00:06:14 +0530

    Delete account-service directory

--- account-service/src/main/java/com/dnb/accountservice/enums/AccountType.java
+++
@@ -1,5 +0,0 @@
-package com.dnb.accountservice.enums;
-
-public enum AccountType {
-	Saving,Current
-}
--- account-service/src/main/java/com/dnb/accountservice/payload/request/AmountRequest.java
+++
@@ -1,8 +0,0 @@
-package com.dnb.accountservice.payload.request;
-
-import lombok.Data;
-
-@Data
-public class AmountRequest {
- long amount;
-}
